9ce1766a8866eb3204bc7d4130dd1983b2f1aafa
bkilian <kilian.boudeaud@ynov.com>
2025-11-14 10:44:01 +0100

    ajout modif color

--- main.go
+++ main.go
@@ -10,3 +10,17 @@ func main() {
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+...  
+func ColorHandler(w http.ResponseWriter, r *http.Request) {  
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")  
+}  
+...  
+  
+func main() {  
+...  
+	http.HandleFunc("/color", ColorHandler)  
+...  
+}  
+
+chmod +x ./script  
+./fake-colleague.sh <git-url> 
